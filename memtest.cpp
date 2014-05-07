ef31976c7ed8f0419057ec891d33db5c16c41639
David McLaren <sirusblk@gmail.com>
2014-05-06 19:21:45 -0700

    Read in input file and store as array of structs

--- memtest.cpp
+++ memtest.cpp
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
 
 struct process
 {
@@ -10,24 +15,68 @@ struct process
 	int time_start;
 	int time_end;
 	int num_block;
-	int block_size[];
+	vector<int> block_size;
 };
+
+//Prototypes
+void readInFile(string);
 
 int main(int argc, char** argv)
 {
 	// Check the command line arguments
 	if(argc < 2)
 	{
-		fprintf(stderr, "USAGE: %s <MEMSIZE_KBYTES> <PAGE_SIZE>\n", argv[0]);
+		fprintf(stderr, "USAGE: %s <MEMSIZE_KBYTES> <PAGE_SIZE_OPTION>\n", argv[0]);
 		exit(-1);
 	}
 
 	//Variables
 	int memSize = atoi(argv[1]);
-	int pageSize = atoi(argv[2]);
+	int pageSize = atoi(argv[2]) * 100;
+
+	if (pageSize == 300)
+		pageSize += 100;
 
 	//DO SOMETHING...
-
+	readInFile("in1.txt");
 
 	return 0;
+}
+
+void readInFile(string input_file)
+{
+	ifstream my_file;
+	my_file.open(input_file);
+
+	int num_processes;
+	int temp;
+
+	my_file >> num_processes;
+	process * my_processes = new process[num_processes];
+
+	for (int i = 0; i < num_processes; ++i)
+	{
+		my_file >> my_processes[i].num;
+		my_file >> my_processes[i].time_start;
+		my_file >> my_processes[i].time_end;
+		my_file >> my_processes[i].num_block;
+
+		for (int j = 0; j < my_processes[i].num_block; ++j)
+		{
+			my_file >> temp;
+			my_processes[i].block_size.push_back(temp);
+		}
+	}
+
+	my_file.close();
+
+	//DEBUG
+	/*
+	cout << "Start Debug...\n";
+	for (int i = 0; i < num_processes; ++i)
+	{
+		cout << my_processes[i].num << " | " << my_processes[i].time_start << " | " << my_processes[i].time_end << " | " << my_processes[i].num_block << endl;
+	}
+	cout << "End Debug...\n";
+	 */	
 }
